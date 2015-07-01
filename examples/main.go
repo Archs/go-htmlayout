9af65bebdefc80f4cba8c820f101d44b4a065844
Your Name <you@example.com>
2015-07-02 01:00:41 +0800

    using lxn/win to write the win32 GUI

---
+++ examples/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"syscall"
+	"unsafe"
+
+	"github.com/Archs/go-htmlayout"
+	"github.com/lxn/win"
+)
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main() {
+	inst := win.GetModuleHandle(nil)
+	r := WinMain(inst)
+	fmt.Println("WinMain函数返回", r)
+}
+
+func WinMain(Inst win.HINSTANCE) int32 {
+	// 1. 注册窗口类
+	atom := MyRegisterClass(Inst)
+	if atom == 0 {
+		fmt.Println("注册窗口类失败:", win.GetLastError())
+		return 0
+	}
+	fmt.Println("注册窗口类成功", atom)
+
+	// 2. 创建窗口
+	// wnd, err := win.CreateWindow("主窗口类", "golang windows 编程",
+	// 	win.WS_OVERLAPPEDWINDOW, 0,
+	// 	win.CW_USEDEFAULT, win.CW_USEDEFAULT, win.CW_USEDEFAULT, win.CW_USEDEFAULT,
+	// 	0, 0, Inst, 0)
+	wnd := win.CreateWindowEx(win.WS_EX_APPWINDOW,
+		syscall.StringToUTF16Ptr("主窗口类"),
+		nil,
+		win.WS_OVERLAPPEDWINDOW|win.WS_CLIPSIBLINGS,
+		win.CW_USEDEFAULT,
+		win.CW_USEDEFAULT,
+		win.CW_USEDEFAULT,
+		win.CW_USEDEFAULT,
+		0,
+		0,
+		Inst,
+		nil)
+	if wnd == 0 {
+		fmt.Println("创建窗口失败", win.GetLastError())
+		return 0
+	}
+	fmt.Println("创建窗口成功", wnd)
+	win.ShowWindow(wnd, win.SW_SHOW)
+	win.UpdateWindow(wnd)
+
+	// 3. 主消息循环
+	var msg win.MSG
+	msg.Message = win.WM_QUIT + 1 // 让它不等于 win.WM_QUIT
+
+	for win.GetMessage(&msg, 0, 0, 0) > 0 {
+		win.TranslateMessage(&msg)
+		win.DispatchMessage(&msg)
+	}
+
+	return int32(msg.WParam)
+}
+
+// func DefWindowProc(hWnd HWND, Msg uint32, wParam, lParam uintptr) uintptr
+func WndProc(hWnd win.HWND, message uint32, wParam uintptr, lParam uintptr) uintptr {
+	var pbHandled gohl.BOOL
+	ret, handled := gohl.ProcNoDefault(uint32(hWnd), message, wParam, lParam)
+	if handled {
+		return uintptr(ret)
+	}
+	if pbHandled != 0 { //先把消息给HTMLayout
+		return uintptr(ret)
+	}
+	switch message {
+	case win.WM_CREATE:
+		if err := gohl.LoadFile(uint32(hWnd), "a.html"); err != nil {
+			println("LoadFile failed", err.Error())
+		}
+	default:
+		return win.DefWindowProc(hWnd, message, wParam, lParam)
+	}
+	return 0
+}
+
+func MyRegisterClass(hInstance win.HINSTANCE) (atom win.ATOM) {
+	var wc win.WNDCLASSEX
+	wc.CbSize = uint32(unsafe.Sizeof(wc))
+	wc.Style = win.CS_HREDRAW | win.CS_VREDRAW
+	wc.LpfnWndProc = syscall.NewCallback(WndProc)
+	wc.CbClsExtra = 0
+	wc.CbWndExtra = 0
+	wc.HInstance = hInstance
+	wc.HbrBackground = win.GetSysColorBrush(win.COLOR_WINDOWFRAME)
+	wc.LpszMenuName = syscall.StringToUTF16Ptr("")
+	wc.LpszClassName = syscall.StringToUTF16Ptr("主窗口类")
+	wc.HIconSm = win.LoadIcon(0, win.MAKEINTRESOURCE(win.IDI_APPLICATION))
+	wc.HIcon = win.LoadIcon(0, win.MAKEINTRESOURCE(win.IDI_APPLICATION))
+	wc.HCursor = win.LoadCursor(0, win.MAKEINTRESOURCE(win.IDC_ARROW))
+
+	return win.RegisterClassEx(&wc)
+}
